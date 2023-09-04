b2d47b56caa3c028a2e08f4375c815bbbf6de7f6
Sandi-09 <iamsandi09@gmail.com>
2023-09-04 21:09:51 +0530

    modified files

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-;
 
         Map<Integer,String> map = new HashMap<>();
         map.put(1,"jejjej");
